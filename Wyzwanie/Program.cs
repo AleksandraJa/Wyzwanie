0650722d82576e5b494d1cda1e3e01c39ed7a8a7
Ale Jaga <aleksandra.jagiello@outlook.com>
2024-09-25 11:01:57 +0200

    Zadadanie domowe z dnia 4

--- Wyzwanie/Program.cs
+++ Wyzwanie/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var name = "Ola";
+var sex = "female";
+var age = 26;
+
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("kobieta ponizej 30 lat");
+}
+else if(name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
