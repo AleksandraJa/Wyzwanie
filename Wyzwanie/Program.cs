eba6572ff440c957a9bb59189ab9b49dd43e5fa5
Ale Jaga <aleksandra.jagiello@outlook.com>
2024-10-11 23:40:04 +0200

    Zadanie z dnia 5

--- Wyzwanie/Program.cs
+++ Wyzwanie/Program.cs
@@ -1,18 +1,76 @@
 ﻿
-var name = "Ola";
-var sex = "female";
-var age = 26;
 
 
-if (sex == "female" && age < 30)
+
+int number = 9530;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToCharArray();
+
+var counter = new int [10];
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in letters)
+
 {
-    Console.WriteLine("kobieta ponizej 30 lat");
+    if (letter == '0')
+    {
+        counter[0]++;
+    }
+    else if (letter == '1')
+    {
+        counter[1]++;
+    }
+    if (letter == '2')
+    {
+        counter[2]++;
+    }
+    else if (letter == '3')
+    {
+        counter[3]++;
+    }
+    if (letter == '4')
+    {
+        counter[4]++;
+    }
+    else if (letter == '5')
+    {
+        counter[5]++;
+    }
+    if (letter == '6')
+    {
+        counter[6]++;
+    }
+    else if (letter == '7')
+    {
+        counter[7]++;
+    }
+    if (letter == '8')
+    {
+        counter[8]++;
+    }
+    else if (letter == '9')
+    {
+        counter[9]++;
+    }
+
 }
-else if(name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa lat 30");
-}
-else if (sex == "male" && age < 18)
-{
-    Console.WriteLine("Niepelnoletni mezczyzna");
-}
+Console.WriteLine("0 =>" + counter0);
+Console.WriteLine("1 =>" + counter1);
+Console.WriteLine("2 =>" + counter2);
+Console.WriteLine("3 =>" + counter3);
+Console.WriteLine("4 =>" + counter4);
+Console.WriteLine("5 =>" + counter5);
+Console.WriteLine("6 =>" + counter6);
+Console.WriteLine("7 =>" + counter7);
+Console.WriteLine("8 =>" + counter8);
+Console.WriteLine("9 =>" + counter9);
